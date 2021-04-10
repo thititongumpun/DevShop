a2072651a01645dd4e5eec2c5ff4cb49d27eb6d4
DESKTOP-UD5VQCF\thiti <thiti.adev@gmail.com>
2021-04-10 16:54:17 +0700

    DbUp

--- DevShop-Backend/devshops.Domain/ViewModels/Developer/DeveloperViewModel.cs
+++ DevShop-Backend/devshops.Domain/ViewModels/Developer/DeveloperViewModel.cs
@@ -9,7 +9,7 @@ namespace devshops.Domain.Developer.ViewModels
         public int DeveloperId { get; set; }
         public string DeveloperName { get; set; }
         public string Email { get; set; }
-        public string GithubUrl { get; set; }
+        public string GithubUrl { get; set; } 
         public DateTime JoinedDate { get; set; }
 
         public string Status { get; set; }
