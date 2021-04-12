f18e7110cadfd54c50e3b9f86c39e6607635fe08
DESKTOP-UD5VQCF\thiti <thiti.adev@gmail.com>
2021-04-12 23:01:20 +0700

    log

--- DevShop-Backend/devshops.Api/Controllers/PositionController.cs
+++ DevShop-Backend/devshops.Api/Controllers/PositionController.cs
@@ -2,6 +2,7 @@
 using devshops.Domain.ViewModels.Position;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,15 +15,18 @@ namespace devshops.Api.Controllers
     public class PositionController : ControllerBase
     {
         protected readonly IPositionService _positionService;
-        public PositionController(IPositionService positionService)
+        protected readonly ILogger<PositionController> _logger;
+        public PositionController(IPositionService positionService, ILogger<PositionController> logger)
         {
             _positionService = positionService;
+            _logger = logger;
         }
 
         [HttpGet]
         public async Task<ActionResult<PositionViewModel>> GetAllPositions()
         {
             var position = await _positionService.GetAllPositions();
+            _logger.LogInformation("Getting all Positions");
             return Ok(position);
         }
 
@@ -30,6 +34,7 @@ namespace devshops.Api.Controllers
         public IActionResult AddPosition([FromBody] PositionCreateModel position)
         {
             _positionService.AddPosition(position);
+            _logger.LogInformation("Insert Position");
             return Ok(position);
         }
 
@@ -37,6 +42,7 @@ namespace devshops.Api.Controllers
         public IActionResult UpdatePosition([FromBody] PositionViewModel position)
         {
             _positionService.UpdatePosition(position);
+            _logger.LogInformation("Update Position");
             return Ok(position);
         }
 
@@ -44,6 +50,7 @@ namespace devshops.Api.Controllers
         public IActionResult DeletePosition(int id)
         {
             _positionService.DeletePosition(id);
+            _logger.LogInformation($"Delete Position {id}");
             return NoContent();
         }
     }
