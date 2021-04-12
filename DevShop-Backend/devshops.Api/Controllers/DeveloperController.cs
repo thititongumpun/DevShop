93327dc28fba69b11c2abf33766dac7937040902
DESKTOP-UD5VQCF\thiti <thiti.adev@gmail.com>
2021-04-12 20:51:19 +0700

    test logger

--- DevShop-Backend/devshops.Api/Controllers/DeveloperController.cs
+++ DevShop-Backend/devshops.Api/Controllers/DeveloperController.cs
@@ -2,6 +2,7 @@ using System.Threading.Tasks;
 using devshops.Core.Developer;
 using devshops.Domain.Developer.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace devshops.Api.Controllers
 {
@@ -10,15 +11,18 @@ namespace devshops.Api.Controllers
     public class DeveloperController : ControllerBase
     {
         protected readonly IDeveloperService _developerService;
-        public DeveloperController(IDeveloperService developerService)
+        protected readonly ILogger<DeveloperController> _logger;
+        public DeveloperController(IDeveloperService developerService, ILogger<DeveloperController> logger)
         {
             _developerService = developerService;
+            _logger = logger;
         }
 
         [HttpGet]
         public async Task<ActionResult<DeveloperViewModel>> GetAllDevelopers()
         {
             var developers = await _developerService.GetAllDevelopers();
+            _logger.LogInformation("Geting all developers ...");
             return Ok(developers);
         }
 
