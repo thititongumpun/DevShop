b8e23970b8e6f032be58e563ff085e033811a8d1
Thiti <thiti.adev@gmail.com>
2021-04-13 11:20:52 +0700

    setup swagger

--- DevShop-Backend/devshops.Api/Startup.cs
+++ DevShop-Backend/devshops.Api/Startup.cs
@@ -51,9 +51,14 @@ namespace devshops.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "devshops.Api v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "devshops.Api v1");
+                c.RoutePrefix = string.Empty;
+            });
+            
 
             app.UseHttpsRedirection();
 
