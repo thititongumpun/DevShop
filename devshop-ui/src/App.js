b1cf85ed7f95669d540eef52b2cccdb8188ce357
DESKTOP-UD5VQCF\thiti <thiti.adev@gmail.com>
2021-04-16 22:37:52 +0700

    init react

---
+++ devshop-ui/src/App.js
@@ -0,0 +1,11 @@
+import { StyledContainer } from './components/Styles';
+
+function App() {
+  return (
+    <StyledContainer>
+      <p>yo</p>
+    </StyledContainer>
+  );
+}
+
+export default App;
---
+++ devshop-ui/src/components/Styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const colors = {
+  primary: '#fff',
+  theme: '#BE185D',
+  light1: '#F3F4F6',
+  light2: '#E5E7EB',
+  dark1: '#1F2937',
+  dark2: '#4B5563',
+  dark3: '#9CA3AF',
+  red: '#DC2626'
+}
+
+export const StyledContainer = styled.div`
+  margin: 0;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(0deg, rgba(0,0,0,0.6), rgba(0,0,0,0.6));
+`;
