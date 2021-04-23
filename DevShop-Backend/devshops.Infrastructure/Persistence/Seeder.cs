fcf9bdd366658033ffd934d09d729d225f862330
DESKTOP-UD5VQCF\thiti <thiti.adev@gmail.com>
2021-04-23 22:44:26 +0700

    seed admin user

---
+++ DevShop-Backend/devshops.Infrastructure/Persistence/Seeder.cs
@@ -0,0 +1,54 @@
+﻿using devshops.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace devshops.Infrastructure.Persistence
+{
+    public class Seeder
+    {
+        private AppDbContext context;
+        public Seeder(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async void SeedAdminUser()
+        {
+            var user = new ApplicationUser
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = "Admin",
+                NormalizedUserName = "ADMIN",
+                Email = "Admin@localhost.com",
+                NormalizedEmail = "ADMIN@LOCALHOST.COM",
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+
+            var roleStore = new RoleStore<IdentityRole>(context);
+
+            if (!context.Roles.Any(r => r.Name == "admin"))
+            {
+                await roleStore.CreateAsync(new IdentityRole { Name = "admin", NormalizedName = "admin" });
+            }
+
+            if (!context.Users.Any(u => u.UserName == user.UserName))
+            {
+                var password = new PasswordHasher<ApplicationUser>();
+                var hashed = password.HashPassword(user, "12345");
+                user.PasswordHash = hashed;
+                var userStore = new UserStore<ApplicationUser>(context);
+                await userStore.CreateAsync(user);
+                await userStore.AddToRoleAsync(user, "admin");
+            }
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
