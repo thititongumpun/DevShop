f414b6d8d64af049c3bc4752797b831460c08cc0
DESKTOP-UD5VQCF\thiti <thiti.adev@gmail.com>
2021-04-27 20:34:21 +0700

    redis

---
+++ DevShop-Backend/devshops.Infrastructure/Redis/IResponseCacheService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace devshops.Infrastructure.Redis
+{
+    public interface IResponseCacheService
+    {
+        Task CacheResponseAsync(string cacheKey, object response, TimeSpan timeTimeLive);
+        Task<string> GetCachedResponseAsync(string cacheKey);
+    }
+}
