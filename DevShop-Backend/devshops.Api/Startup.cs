d30e2177e9b79aa9e4d758b7c9ba45a41b69ad5a
DESKTOP-UD5VQCF\thiti <thiti.adev@gmail.com>
2021-05-10 23:00:18 +0700

    connectionString

--- DevShop-Backend/devshops.Api/Startup.cs
+++ DevShop-Backend/devshops.Api/Startup.cs
@@ -64,7 +64,7 @@ namespace devshops.Api
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            
             seeder.SeedAdminUser();
 
             app.UseSwaggerDocumentation();
